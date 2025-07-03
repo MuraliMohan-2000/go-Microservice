65651bd5eeb3dfc087684604d0fb297b067f65de
murali-mohan <muralimohan.sampath@outlook.com>
2025-07-03 15:20:04 -0400

    Account service functions

--- account/server.go
+++ account/server.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"net"
 
+	pb "dev.murali.go-microservice/account/pb"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
 	service Service
 }
 
@@ -18,34 +21,53 @@ func ListenGRPC(s Service, port int) error {
 	if err != nil {
 		return err
 	}
-
 	serv := grpc.NewServer()
-	pb.(serv, )
-    reflection.Register(serv)
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service:                           s,
+	})
+	reflection.Register(serv)
 	return serv.Serve(lis)
 }
 
-func (s *grpcServer) PostAccount(ctx context.Context, r *pb.)(*pb., error){
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
 	a, err := s.service.PostAccount(ctx, r.Name)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id:   a.ID,
+		Name: a.Name,
+	}}, nil
 }
 
-func (s *grpcServer) GetAccount(ctx context.Context, r *pb.)(*pb., error){
-	a, err := s.service.GetAccount(ctx, r.ID)
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	a, err := s.service.GetAccount(ctx, r.Id)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
+	return &pb.GetAccountResponse{
+		Account: &pb.Account{
+			Id:   a.ID,
+			Name: a.Name,
+		},
+	}, nil
 }
 
-func (s *grpcServer) GetAccounts (ctx context.Context, r *pb.)(*pb., error){
-	a, err := s.service.GetAccounts(ctx, r.ID)
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+	res, err := s.service.GetAccounts(ctx, r.Skip, r.Take)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.{}
-
+	accounts := []*pb.Account{}
+	for _, p := range res {
+		accounts = append(
+			accounts,
+			&pb.Account{
+				Id:   p.ID,
+				Name: p.Name,
+			},
+		)
+	}
+	return &pb.GetAccountsResponse{Accounts: accounts}, nil
 }
--- account/service.go
+++ account/service.go
@@ -21,7 +21,7 @@ type accountService struct {
 	repository Repository
 }
 
-func newService(r Repository) Service {
+func NewService(r Repository) Service {
 	return &accountService{r}
 }
 
@@ -33,18 +33,16 @@ func (s *accountService) PostAccount(ctx context.Context, name string) (*Account
 	if err := s.repository.PutAccount(ctx, *a); err != nil {
 		return nil, err
 	}
-
 	return a, nil
 }
 
 func (s *accountService) GetAccount(ctx context.Context, id string) (*Account, error) {
-	return s.repository.GetAccountById(ctx, id)
+	return s.repository.GetAccountByID(ctx, id)
 }
 
 func (s *accountService) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
 	if take > 100 || (skip == 0 && take == 0) {
 		take = 100
 	}
-
 	return s.repository.ListAccounts(ctx, skip, take)
 }
