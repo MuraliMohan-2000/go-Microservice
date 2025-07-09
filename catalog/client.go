8e73d54becd5ed5694fb7957378a23581e6b5c45
murali-mohan <muralimohan.sampath@outlook.com>
2025-07-08 21:49:12 -0400

    Catalog service functionalities completed

---
+++ catalog/client.go
@@ -0,0 +1,100 @@
+package catalog
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	pb "dev.murali.go-microservice/catalog/pb"
+)
+
+type client struct {
+	conn    *grpc.ClientConn
+	service pb.CatalogServiceClient
+}
+
+func NewClient(url string) (*client, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	c := pb.NewCatalogServiceClient(conn)
+	return &client{conn, c}, nil
+}
+
+func (c *client) close() {
+	c.conn.Close()
+}
+
+func (c *client) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	r, err := c.service.PostProduct(
+		ctx,
+		&pb.PostProductRequest{
+			Name:        name,
+			Description: description,
+			Price:       price,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &Product{
+		ID:          r.Product.Id,
+		Name:        r.Product.Name,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+
+}
+
+func (c *client) GetProduct(ctx context.Context, id string) (*Product, error) {
+
+	r, err := c.service.GetProduct(
+		ctx,
+		&pb.GetProductRequest{
+			Id: id,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Product{
+		ID:          r.Product.Id,
+		Name:        r.Product.Name,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+
+}
+
+func (c *client) GetProducts(ctx context.Context, ids []string, skip uint64, take uint64, query string) ([]Product, error) {
+	r, err := c.service.GetProducts(
+		ctx,
+		&pb.GetProductsRequest{
+			Ids:   ids,
+			Skip:  skip,
+			Take:  take,
+			Query: query,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	products := []Product{}
+	for _, p := range r.Products {
+		products = append(products, Product{
+			ID:          p.Id,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		})
+	}
+
+	return products, nil
+
+}
